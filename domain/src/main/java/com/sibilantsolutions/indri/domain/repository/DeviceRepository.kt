c30bed9c20b9a59028e52e7cdc9845e72c50bbce
Joel Geiger <joel@sibilantsolutions.com>
2018-01-11 19:28:57 -0500

    Add a 'domain' module.

---
+++ domain/src/main/java/com/sibilantsolutions/indri/domain/repository/DeviceRepository.kt
@@ -0,0 +1,14 @@
+package com.sibilantsolutions.indri.domain.repository
+
+import io.reactivex.Flowable
+
+/**
+* Created by jt on 1/11/18.
+*/
+class DeviceRepository {
+
+    fun devices() : Flowable<String> {
+        TODO("Coming soon!")
+    }
+
+}
